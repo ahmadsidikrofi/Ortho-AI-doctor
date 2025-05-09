01aef458589887ecf840d00a8556d2833e2d316b
Rofi <rofidragon71@gmail.com>
2025-05-09 14:37:16 +0700

    Update API Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -30,7 +30,7 @@ export default function Chat() {
   
     try {
       setMessages((prevMessages) => [...prevMessages, userMessage, typingPlaceholder])
-      const response = await fetch(`http://127.0.0.1:8000/ask`, {
+      const response = await fetch(`https://orthoai.maorthonet.com/ask`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query: input }),
